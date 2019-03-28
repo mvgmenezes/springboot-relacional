865736d289af36270101bf1795fb69407f674c6c
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-27 22:45:15 -0300

    Incluindo banco h2 e criando a entidade

--- src/main/java/com/mmenezes/springbootrelacional/api/resources/CategoriaResource.java
+++ src/main/java/com/mmenezes/springbootrelacional/api/resources/CategoriaResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/api/categorias")
 public class CategoriaResource {
-
+ 
 	
 	@GetMapping()
 	public String listar() {
--- src/main/java/com/mmenezes/springbootrelacional/entity/Categoria.java
+++ src/main/java/com/mmenezes/springbootrelacional/entity/Categoria.java
@@ -2,6 +2,13 @@ package com.mmenezes.springbootrelacional.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable{
 	
 	/**
@@ -9,6 +16,8 @@ public class Categoria implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private Integer id;
 	private String nome;
 	
